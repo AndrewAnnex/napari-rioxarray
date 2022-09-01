ba26d6019c1075f60c4a0d92079642e7bfc8bbe7
Andrew Annex <ama6fy@virginia.edu>
2022-08-31 18:59:29 -0700

    added small test data

--- src/napari_rioxarray/_tests/test_reader.py
+++ src/napari_rioxarray/_tests/test_reader.py
@@ -1,29 +1,24 @@
-import numpy as np
+import importlib.resources
 
 from napari_rioxarray import napari_get_reader
+
+from . import data
 
 
 # tmp_path is a pytest fixture
 def test_reader(tmp_path):
     """An example of how you might test your plugin."""
 
-    # write some fake data using your supported file format
-    my_test_file = str(tmp_path / "myfile.npy")
-    original_data = np.random.rand(20, 20)
-    np.save(my_test_file, original_data)
-
-    # try to read it back in
-    reader = napari_get_reader(my_test_file)
-    assert callable(reader)
-
-    # make sure we're delivering the right format
-    layer_data_list = reader(my_test_file)
-    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-    layer_data_tuple = layer_data_list[0]
-    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
-
-    # make sure it's the same as it started
-    np.testing.assert_allclose(original_data, layer_data_tuple[0])
+    # get
+    with importlib.resources.path(data, "S2A_MSIL1C_20170102T111442_N0204_R137_T30TXT_20170102T111441_TCI_cloudoptimized_512_100m.tif") as my_test_file:
+        # try to read it back in
+        reader = napari_get_reader(my_test_file)
+        assert callable(reader)
+        # make sure we're delivering the right format
+        layer_data_list = reader(my_test_file)
+        assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+        layer_data_tuple = layer_data_list[0]
+        assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
 
 
 def test_get_reader_pass():
